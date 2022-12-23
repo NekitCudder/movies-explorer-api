961ec30c6cd02cabe0b75127ce7e83528542e632
Nikita Boev <nekitcudder@yandex.ru>
2022-12-23 19:00:59 +0300

    #1 fix сделать новый выброс ошибки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new UnauthorizedError('Ошибка авторизации #2.'));
+    throw new UnauthorizedError('Ошибка авторизации #2.');
   }
   req.user = payload;
   next();
